b933afdda760e5253f479e4386fb0b96af2a9781
Elliott Sánchez León <raptordroid15@gmail.com>
2022-10-17 21:50:39 -0500

    Cuarto commit

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -2,15 +2,67 @@
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciarCarrito');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
+let listadoCarrito = [];
 
 const agregarCurso = (e) => {
     e.preventDefault();
-    //e.target.classList.contains('agregar-carrito')
-    if(e.target.classList.contains(agregar-carrito)){
-        console.log("Presionaste el botón");
+    if(e.target.classList.contains('agregar-carrito')){
+        const curso = e.target.parentElement.parentElement;
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            id: curso.querySelector('.agregar.carrito').getAttribute('data-id'),
+            cantidad: 1
+        }
+        agregarCarrito(infoCurso);
     }
+}
+
+const agregarCarrito = curso => {
+    //console.log("Curso a agregar")
+    //console.log(curso.id)
+    //console.log("Listado de cursos")
+    //listadoCarrito.forEach(curso => console.log(curso.id)); 
+    if (listadoCarrito.some(cursoInCarrito=> cursoInCarrito.id===curso.id)){
+    let carrito = listadoCarrito.map(cursoInCarrito=>{
+        if(cursoInCarrito.id==curso.id){
+            cursoInCarrito.cantidad++;
+            return cursoInCarrito;
+        }else {
+            return cursoInCarrito;
+        }
+    })
+    lis
+    tadoCarrito = [...carrito];
+} else{
+    listadoCarrito= [...listadoCarrito,curso];
+}
+    console.log(listadoCarrito);
+    generaHTML();
+}
+
+const generaHTML = () => {
+    vaciarCarritoBtn();
+    listadoCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        const cursoHTML = `
+        <td>
+            <img src="${curso.imagen}"width=100>
+        </td>
+        <td>${curso.nombre}</td>
+        <td>${curso.precio}</td>
+        <td>${curso.cantidad}</td>
+        `;
+        row.innerHTML = cursoHTML;
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+const vaciarCarrito = () => {
+    contenedorCarrito.innerHTML = '';
 }
 
 const cargarEventListener = () => {
